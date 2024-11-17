125aedcfef83f3294e3b472d70bec23c1b17035f
Abdullah-Bakhshov <140121021+Abdullah-Bakhshov@users.noreply.github.com>
2024-11-17 03:08:06 +0000

    grammar

--- main.cpp
+++ main.cpp
@@ -6,30 +6,30 @@
 
 int main(){
 
-    // instance of the api server
+    // Instance of the api server I'm creating
     crow::SimpleApp server;
 
-    // route for root
+    // Route for root
     CROW_ROUTE(server, "/")
     ([](){
         return "this is root";
     });
 
-    // route for video processing
+    // Route for video processing
     CROW_ROUTE(server, "/video_process")
     ([](){
         return "this is where machine learning will take place";
     });
 
-    // route for video data
+    // Route for video data
     CROW_ROUTE(server, "/video_meta")
     ([](){
         return "this is where we call on the data base for video data";
     });
 
-    // set default port to default ~ 80 and used threading to handle multiple calls
+    // Runs server, set default port to default ~ 80 and used threading to handle multiple calls
     server.port(80).multithreaded().run();
 
-    // success
+    // Success
     return 0;
 }
