fb43635c3bc1742844b74eb488c3c1762d6f218b
Abdullah-Bakhshov <140121021+Abdullah-Bakhshov@users.noreply.github.com>
2024-11-24 02:05:47 +0000

    made a working way to upload small videos

--- server.cpp
+++ server.cpp
@@ -1,29 +1,34 @@
 #include "crow.h"
 #include <asio.hpp>
+#include <fstream>
+#include <iostream>
+#include <filesystem> 
 
-// request data we are then processing, middleware
-struct ContextData{
+// // request data we are then processing, middleware
+// // struct ContextData{
 
-    // local data where we store the parsed data, intermediate for the other service to handle
-    struct context{};
+// //     // local data where we store the parsed data, intermediate for the other service to handle
+// //     struct context{};
 
-    // takes input to api and standerdises it
-    void before_handle(crow::request &req, crow::response &res, context &ctx){
+// //     // takes input to api and standerdises it
+// //     void before_handle(crow::request &req, crow::response &res, context &ctx){
 
-    }
+// //     }
 
-    // after we are finished with intermediate service we then send it back to client in this format
-    void after_handle(crow::request &req, crow::response &res, context &ctx){
+// //     // after we are finished with intermediate service we then send it back to client in this format
+// //     void after_handle(crow::request &req, crow::response &res, context &ctx){
 
-    }
-};
+// //     }
+// // };
 
 
 
 int main(){
 
     // Instance of the api server I'm creating
-    crow::App<ContextData> server;
+    // crow::App<ContextData> server;
+    crow::SimpleApp server; // testing
+
 
     // Route for root
     CROW_ROUTE(server, "/")
@@ -41,9 +46,47 @@ int main(){
     
     // Route for uploading video processing - machine learning
 
-    CROW_ROUTE(server, "/uploading_to_ml_model").methods(crow::HTTPMethod::PUT)
-    ([](){
-        return "this is where machine learning will take place"; // this will give error we want to return a request to a cv2 model
+    CROW_ROUTE(server, "/uploading_to_ml_model").methods(crow::HTTPMethod::POST)
+        // making a request to the server, const makes it static and therefore can't be changed,
+        // & means its passed by reference
+    ([](const crow::request& req){
+
+        // the request we got to the server is then parsed as a multipart message, 
+        crow::multipart::message msg(req);
+
+        // checking if the request contains any parts
+        if (msg.parts.empty()) {
+            return crow::response(400, "the file send was empty or not found");
+        }
+
+
+        // define a path to save the video to and the name of the file
+        const std::string temp_file = "uploaded_video.mp4";
+
+        // Save the file content to the filesystem as a binary file
+        std::ofstream out(temp_file, std::ios::binary);
+
+        // iterating through the parts of the message, msg.parts is a vector of parts
+        for (const auto& part: msg.parts){
+            // check if the header is correct => video/mp4
+            // we are checking the map for the key with the value of "Content-Type"
+            const std::string content_type = part.get_header_object("Content-Type").value;
+            // we check if we found the header and if its value is a video file
+            if (content_type.empty() || content_type != "video/mp4") {
+                return crow::response(400, "the file is not a video file");
+            }
+            // if we dont get a output from file we return a 500 error
+            if (!out) {
+                return crow::response(500, "Failed to save the file to disk.");
+            } 
+            // we write the body of the part to the file
+            out.write(part.body.data(), part.body.size());
+        }
+        // closing the file
+        out.close();
+
+        // SUCCESS
+        return crow::response(200,"video has been succesfully been uploaded");
     });
 
     // Route for retrieving the video processing
@@ -54,7 +97,7 @@ int main(){
     }); 
 
 
-
+    
 
 
 
