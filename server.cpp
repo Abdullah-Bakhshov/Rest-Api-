6fa2a51b90068da7a4de82dd4ad3c18c223e82eb
Abdullah-Bakhshov <140121021+Abdullah-Bakhshov@users.noreply.github.com>
2024-11-25 23:08:34 +0000

    formating

--- server.cpp
+++ server.cpp
@@ -41,62 +41,63 @@ int main(){
     
     // Route for uploading video processing - machine learning
 
-CROW_ROUTE(server, "/uploading_to_ml_model").methods(crow::HTTPMethod::POST)
-([](const crow::request& req) {
-    try {
+    CROW_ROUTE(server, "/uploading_to_ml_model").methods(crow::HTTPMethod::POST)
+    ([](const crow::request& req) {
+        try {
 
-        // check the type of content we are recieveing set by client
-        std::cout << "Received request: " << req.get_header_value("Content-Type") << std::endl;
-        
-        // if we don't find anything we get a value of -1 which is npos, negative position , position not found
-        // if we find the string we are looking for we get a value greater than 0
-        if (req.get_header_value("Content-Type").find("multipart/form-data") == std::string::npos) {
-            return crow::response(400, "Didn't recieve type multipart/form-data");
-        }
-
-        // we are parsing the request data as a multipart message, puts the parts in a vector
-        crow::multipart::message msg(req);
-
-        // if we dont have any parts that means we have recieved no data just a call to the api
-        if (msg.parts.empty()) {
-            return crow::response(400, "No parts in the request.");
-        }
-
-        // we are setting the file path and name to store the data
-        const std::string temp_file = "uploaded_video.mov";
-
-        // we are opening the file at temp_file and writing binary data into it
-        std::ofstream out(temp_file, std::ios::binary);
-
-
-        // if we don't have a file to open we return a 500 error
-        if (!out) {
-            return crow::response(500, "Failed to open file for writing.");
-        }
-
-        // we are iterating over the different parts of the message to check if the content type is video quicktime or if it is empty
-        // we then give a error if we don't get expected type
-        // otherwise we then write the data to the file set by the size of the body being put in
-        for (const auto& part : msg.parts) {
-            const std::string content_type = part.get_header_object("Content-Type").value;
-            if (content_type.empty() || content_type != "video/quicktime") {
-                return crow::response(400, "Invalid file type.");
+            // check the type of content we are recieveing set by client
+            std::cout << "Received request: " << req.get_header_value("Content-Type") << std::endl;
+            
+            // if we don't find anything we get a value of -1 which is npos, negative position , position not found
+            // if we find the string we are looking for we get a value greater than 0
+            if (req.get_header_value("Content-Type").find("multipart/form-data") == std::string::npos) {
+                return crow::response(400, "Didn't recieve type multipart/form-data");
             }
-            out.write(part.body.data(), part.body.size());
+
+            // we are parsing the request data as a multipart message, puts the parts in a vector
+            crow::multipart::message msg(req);
+
+            // if we dont have any parts that means we have recieved no data just a call to the api
+            if (msg.parts.empty()) {
+                return crow::response(400, "No parts in the request.");
+            }
+
+            // we are setting the file path and name to store the data
+            const std::string temp_file = "uploaded_video.mov";
+
+            // we are opening the file at temp_file and writing binary data into it
+            std::ofstream out(temp_file, std::ios::binary);
+
+
+            // if we don't have a file to open we return a 500 error
+            if (!out) {
+                return crow::response(500, "Failed to open file for writing.");
+            }
+
+            // we are iterating over the different parts of the message to check if the content type is video quicktime or if it is empty
+            // we then give a error if we don't get expected type
+            // otherwise we then write the data to the file set by the size of the body being put in
+            for (const auto& part : msg.parts) {
+                const std::string content_type = part.get_header_object("Content-Type").value;
+                if (content_type.empty() || content_type != "video/quicktime") {
+                    return crow::response(400, "Invalid file type.");
+                }
+                out.write(part.body.data(), part.body.size());
+            }
+
+            // we then close the file after this is completed
+            out.close();
+
+
+            // SUCCESS!!!
+            return crow::response(200, "Video uploaded successfully.");
+            
+        } catch (const std::exception& e) {
+
+            // this is if we get a error while we process it such as segmentation fault ect
+            return crow::response(500, std::string("Server error: ") + e.what());
         }
-
-        // we then close the file after this is completed
-        out.close();
-
-
-        // SUCCESS!!!
-        return crow::response(200, "Video uploaded successfully.");
-    } catch (const std::exception& e) {
-
-        // this is if we get a error while we process it such as segmentation fault ect
-        return crow::response(500, std::string("Server error: ") + e.what());
-    }
-});
+    });
 
 
     // Route for retrieving the video processing
